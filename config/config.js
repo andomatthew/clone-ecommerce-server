39a0e87fe604744bfe9acec30d680fc4f8be1f43
andomatthew <alexandro.matthew@gmail.com>
2021-02-21 17:27:52 +0800

    set config ssl

--- config/config.js
+++ config/config.js
@@ -18,6 +18,11 @@ module.exports = {
     "dialect": "postgres"
   },
   "production": {
-    "use_env_variable": "DATABASE_URL"
+    "use_env_variable": "DATABASE_URL",
+    "dialectOptions": {
+      "ssl": {
+        "rejectUnauthorized": false
+      }
+    }
   }
 }
